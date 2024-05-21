e8c59ba635b3d94b569515ab2b79da83e0581721
codingspider <engrokon.rok@gmail.com>
2024-05-21 21:46:34 +0600

    design changes

--- templates/home.php
+++ templates/home.php
@@ -12,16 +12,16 @@ $add_url = home_url(add_query_arg(NULL, NULL)) . '&sub_page=add';
 </div>
 
 <?php foreach( $rules as $rule ): ?>
-    <div class="tsm-col-4">
-      <div class="tsm-first-box tsm-card">
+<div class="tsm-col-4">
+    <div class="tsm-first-box tsm-card">
         <h2>
             <?php echo esc_html( get_post_meta( $rule->ID, 'rule_title', true ) /* $rule->post_title */); ?>
         </h2>
         <p>Genera rule to allow the customer free shipping, when they have more than $1025 ...</p>
-		<div>
+        <div>
             <a class="button-left" href="">Edit</a>
             <a class="button-right" href="">Delete</a>
         </div>
-      </div>
     </div>
+</div>
 <?php endforeach; ?>
--- templates/settings-layout.php
+++ templates/settings-layout.php
@@ -3,16 +3,16 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <div class="tsm-body">
-    
-<div class="tsm-row">
-    <div class="tsm-col-3 tsm-left-menu">
-        <div>
-            <h2>Shipping Manager</h2>
-            <p>by THEMEPASTE</p>
+
+    <div class="tsm-row">
+        <div class="tsm-col-3 tsm-left-menu">
+            <div>
+                <h2>Shipping Manager</h2>
+                <p>by THEMEPASTE</p>
+            </div>
+        </div>
+        <div class="tsm-col-9 tsm-main">
+            <?php include( get_sub_page() . '.php' ); ?>
         </div>
     </div>
-    <div class="tsm-col-9 tsm-main">
-        <?php include( get_sub_page() . '.php' ); ?>
-    </div>
-</div>
 </div>
